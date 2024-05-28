2272e74682e4c82d249e34be8da92b80f121bfa1
Yelizaveta Dankova (Данькова Єлізавета) <144330950+Lizz-dankova@users.noreply.github.com>
2024-05-28 15:08:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 
+
 def measure_time(func, *args):
     start_time = time.time()
     result = func(*args)
@@ -7,133 +8,125 @@ def measure_time(func, *args):
     execution_time = end_time - start_time
     return result, execution_time
 
+
 def addition(poly_1, poly_2):
-    poly_1 = poly_1.zfill(233)
-    poly_2 = poly_2.zfill(233)
-    result = ''.join(str(int(a) ^ int(b)) for a, b in zip(poly_1, poly_2))
-    return result.lstrip('0') or '0'
+    poly_1 = poly_1.zfill(173)
+    poly_2 = poly_2.zfill(173)
+    c = ['0']
+    for i in range(173):
+        c.append(str(((int(poly_1[i]) if i < len(poly_1) else 0) ^ (int(poly_2[i]) if i < len(poly_2) else 0))))
+    result = ''.join(c)
+    return result.lstrip('0') if len(result) <= 173 else result[(len(result) - 173):]
 
-def binary_division(dividend, divisor):
-    dividend = int(dividend, 2)
-    divisor = int(divisor, 2)
-    quotient = 0
-    while dividend >= divisor:
-        shift = len(bin(dividend)) - len(bin(divisor))
-        dividend ^= (divisor << shift)
-        quotient ^= (1 << shift)
-    remainder = bin(dividend)[2:]
-    return remainder.zfill(233) or '0'
 
-def multiply_polynomials(poly_1, poly_2, mod_poly):
-    poly_1 = poly_1.zfill(233)
-    poly_2 = poly_2.zfill(233)
-    result = [0] * (2 * 233)
-    for i in range(233):
-        if poly_2[232 - i] == '1':
-            for j in range(233):
-                result[i + j] ^= int(poly_1[232 - j])
-    result = ''.join(map(str, result))
-    return binary_division(result, mod_poly)
+def matrix_create():
+    p = 2 * 173 + 1
+    multiplicative_matrix = [[0] * 173 for _ in range(173)]
+    for i in range(173):
+        for j in range(173):
+            if (2 ** i + 2 ** j) % p == 1:
+                multiplicative_matrix[i][j] = 1
+            elif (2 ** i - 2 ** j) % p == 1:
+                multiplicative_matrix[i][j] = 1
+            elif (-1 * 2 ** i + 2 ** j) % p == 1:
+                multiplicative_matrix[i][j] = 1
+            elif (-1 * 2 ** i - 2 ** j) % p == 1:
+                multiplicative_matrix[i][j] = 1
+            else:
+                multiplicative_matrix[i][j] = 0
+    return multiplicative_matrix
 
-def square(poly_1, mod_poly):
-    interleaved = ''.join(bit + '0' for bit in poly_1[:-1]) + poly_1[-1]
-    return binary_division(interleaved, mod_poly)
 
-def power_poly(poly_1, poly_3, mod_poly):
-    result = '1'.zfill(233)
-    for bit in poly_3:
-        if bit == '1':
-            result = multiply_polynomials(result, poly_1, mod_poly)
-        poly_1 = square(poly_1, mod_poly)
+def left_shift(number, shift):
+    num_list = list(number)
+    num_list = num_list[shift:] + num_list[:shift]
+    result = ''
+    for i in num_list:
+        result += i
     return result
 
-def extended_gcd(a, b):
-    if b == 0:
-        return a, 1, 0
-    else:
-        q, r = divmod(a, b)
-        gcd, x1, y1 = extended_gcd(b, r)
-        x = y1
-        y = x1 - q * y1
-        return gcd, x, y
 
-def reverse(poly_1, mod_poly):
-    poly_int = int(poly_1, 2)
-    mod_int = int(mod_poly, 2)
-    gcd, inv, _ = extended_gcd(mod_int, poly_int)
-    if gcd != 1:
-        raise ValueError("Inverse does not exist")
-    inv_bin = bin(inv)[2:]
-    return inv_bin.zfill(233) or '0'
+def mul(a, b):
+    result = ''
+    multiplicative_matrix = matrix_create()
+
+    a = a.zfill(173)
+    b = b.zfill(173)
+
+    for z in range(173):
+        result_1 = [0 for _ in range(173)]
+        result_2 = 0
+        pre_1 = left_shift(a, z)
+        pre_2 = left_shift(b, z)
+
+        for i in range(173):
+            for j in range(173):
+                result_1[i] += int(pre_1[j]) * multiplicative_matrix[j][i]
+            result_1[i] = result_1[i] % 2
+
+        for i in range(173):
+            result_2 += result_1[i] * int(pre_2[i])
+        result_2 = result_2 % 2
+        result += str(result_2)
+
+    return result
+
+
+def square(bitstring):
+    shift = 1
+    length = len(bitstring)
+    shift %= length
+    shifted_bits = bitstring[-shift:] + bitstring[:-shift]
+    return shifted_bits
+
+
+def power(poly_1, poly_3):
+    result = '1'
+    binary_exponent = ''.join(map(str, poly_3))
+    current_power = poly_1
+    for bit in binary_exponent[::-1]:
+        if bit == '1':
+            result = mul(result, current_power)
+        current_power = square(current_power)
+    return result
+
+
+def reverse(poly_1):
+    power_reverse = 2 ** 173 - 2
+    power_reverse = bin(power_reverse)[2:]
+    power_reverse = str(power_reverse)
+    return power(poly_1, power_reverse)
+
 
 def trace(poly):
-    return str(sum(map(int, poly)) % 2)
+    result = 0
+    for char in poly:
+        result += int(char)
+    return result % 2
 
-#mod = '10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000100101001000000000000001'
-
-# Створюємо масив з 234 елементів, заповнений нулями
-binary_representation = ['0'] * 234
-# Встановлюємо "1" на відповідні позиції
-binary_representation[0] = '1'  # для x^233
-binary_representation[224] = '1'  # для x^9
-binary_representation[229] = '1'  # для x^4
-binary_representation[232] = '1'  # для x
-binary_representation[233] = '1'  # для константи 1
-
-# Перетворюємо масив у рядок
-mod = ''.join(binary_representation)
-
-# Виводимо результат
-#print("Binary Representation of mod:", mod)
-
-def binary_to_polynomial(binary_representation):
-    polynomial = ""
-    for i in range(len(binary_representation)):
-        if binary_representation[i] == '1':
-            if i == 0:
-                polynomial += "x^233 + "
-            elif i == 224:
-                polynomial += "x^9 + "
-            elif i == 229:
-                polynomial += "x^4 + "
-            elif i == 232:
-                polynomial += "x + "
-            elif i == 233:
-                polynomial += "1"
-    return polynomial
-
-mod_binary = mod
-mod_polynomial = binary_to_polynomial(mod_binary)
-
-print("Binary Representation of mod:", mod_binary)
-print("Polynomial Representation of mod:", mod_polynomial)
-
-#mod = polynomial_to_binary()
 
 poly1 = str(input("Enter the first polynomial: "))
 poly2 = str(input("Enter the second polynomial: "))
 poly3 = str(input("Enter the third polynomial: "))
 
 
+
 add_result, add_time = measure_time(addition, poly1, poly2)
 print(f'Addition:  {add_result}')
 print(f'Time taken for Addition: {add_time} seconds')
 
-mul_result, mul_time = measure_time(multiply_polynomials, poly1, poly2, mod)
+mul_result, mul_time = measure_time(mul, poly1, poly2)
 print(f'Multiply:  {mul_result}')
 print(f'Time taken for Multiply: {mul_time} seconds')
 
-sq_result, sq_time = measure_time(square, poly1, mod)
+sq_result, sq_time = measure_time(square, poly1)
 print(f'Square:  {sq_result}')
 print(f'Time taken for Square: {sq_time} seconds')
 
-try:
-    reverse_result, reverse_time = measure_time(reverse, poly1, mod)
-    print(f'Reverse:  {reverse_result}')
-    print(f'Time taken for Reverse: {reverse_time} seconds')
-except ValueError as e:
-    print(e)
+reverse_result, reverse_time = measure_time(reverse, poly1)
+print(f'Reverse:  {reverse_result}')
+print(f'Time taken for Reverse: {reverse_time} seconds')
 
-pow_result, pow_time = measure_time(power_poly, poly1, poly3, mod)
+pow_result, pow_time = measure_time(power, poly1, poly3)
 print(f'Power:  {pow_result}')
 print(f'Time taken for Power: {pow_time} seconds')
